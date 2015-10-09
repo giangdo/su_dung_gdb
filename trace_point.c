45fea21ba29a07124d2a0069f4fdef4e0fff62f2
do truong giang <dotruonggiang108@gmail.com>
2015-10-09 17:42:48 +0700

    finish tracepoint almostly

--- trace_point.c
+++ trace_point.c
@@ -4,7 +4,8 @@
 #include <pthread.h>
 
 /************************************************************************************
-													README
+	README
+
 * To compile this program with debug information
 	$gcc trace_point.c -lpthread -g -o trace_point
 
@@ -24,23 +25,19 @@
 	$./trace_point
 *************************************************************************************/
 
-void* thread1Poll(void *arg)
-{
+void* thread1Poll(void *arg) {
 	unsigned int data1;
-	for (data1 = 0; data1 < 1000; data1++)
-	{
+	for (data1 = 0; data1 < 1000; data1++) {
 		usleep(1000*500);
 		fprintf(stdout, "thread1's data:%u\n", data1);
 	}
 	return 0;
 }
 
-void* thread2Poll(void *arg)
-{
+void* thread2Poll(void *arg) {
 	unsigned int data2;
-	for (data2 = 0; data2 < 1000; data2++)
-	{
-		usleep(1000*500);
+	for (data2 = 0; data2 < 1000; data2++) {
+		usleep(1000*1000);
 		fprintf(stdout, "thread2's data:%u\n", data2);
 	}
 	return 0;
@@ -48,11 +45,9 @@ void* thread2Poll(void *arg)
 
 char* endString = "!trace_point example!";
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	// Daemonize
-	switch (fork())
-	{
+	switch (fork()) {
 		case 0:
 			break;
 		case -1:
@@ -61,8 +56,7 @@ int main(int argc, char *argv[])
 		default:
 			exit(0);
 	}
-	if (setsid() == -1) //Create new sesion containing a single (new) process group
-	{
+	if (setsid() == -1) {
 		fprintf(stderr, "setsid failed");
 		exit(1);
 	}
@@ -70,21 +64,16 @@ int main(int argc, char *argv[])
 	//Create thread 1
 	pthread_t thread1;
 	if (pthread_create(&thread1, NULL, thread1Poll, NULL))
-	{
 		fprintf(stderr, "can not create new thread");
-	}
 
 	//Create thread 2
 	pthread_t thread2;
 	if (pthread_create(&thread2, NULL, thread2Poll, NULL))
-	{
 		fprintf(stderr, "can not create new thread");
-	}
 
 	//Wait to join threads
 	if (pthread_join(thread1, NULL) ||
-	    pthread_join(thread2, NULL))
-	{
+	    pthread_join(thread2, NULL)) {
 		fprintf(stderr, "can not join thread");
 		exit(1);
 	}
